764bcc442c949ff83593a817e4e6cd729b6d7ef5
SONNUR TAYFUROĞLU <2014141005@cumhuriyet.edu.tr>
2021-01-24 00:11:46 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<TValue, TKey>
+    {
+        TValue[] values;
+        TKey[] keys;
+
+        public MyDictionary()
+        {
+            values = new TValue[0];
+            keys = new TKey[0];
+        }
+
+        public void Add(TValue val, TKey key)
+        {
+            TValue[] tempVals = values;
+            TKey[] tempKeys = keys;
+
+
+            values = new TValue[values.Length + 1];
+            keys = new TKey[keys.Length + 1];
+
+            for (int i = 0; i < tempVals.Length; i++)
+            {
+                values[i] = tempVals[i];
+                keys[i] = tempKeys[i];
+            }
+
+        }
+        public int Count
+        {
+            get { return values.Length; }
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dictonary = new MyDictionary<int, string>();
+
+            dictonary.Add(1, "Bayburt");
+            dictonary.Add(2, "İstanbul");
+            dictonary.Add(3, "Sivas");
+            dictonary.Add(4, "Antalya");
+
+            Console.WriteLine(dictonary.Count);
+            Console.ReadKey();
+        }
+    }
+}
